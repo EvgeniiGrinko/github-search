db4dccae36fcf6dc6fbae98f698bfba971b11686
Evgenii <jenek0907@mail.ru>
2020-11-15 13:16:57 +0300

    initial commit

---
+++ modules/view.js
@@ -0,0 +1,45 @@
+export class View {
+    constructor(){
+        this.app = document.querySelector('#app');
+        this.title = this.createElement('h1', 'title');
+        this.title.textContent = 'Github Search Users';
+        
+        this.searchLine = this.createElement('div', 'search-line');
+        this.searchInput = this.createElement('input', 'search-input');
+        this.searchCounter = this.createElement('span', 'counter');
+        this.searchLine.append(this.searchInput);
+        this.searchLine.append(this.searchCounter);
+
+        this.main = this.createElement('div', 'main');
+        this.usersWrapper = this.createElement('div', 'users-wrapper');
+        
+        this.usersList = this.createElement('ul', 'users');
+        this.usersWrapper.append(this.usersList);
+        
+        this.loadMore = this.createElement('button', 'btn');
+        this.loadMore.textContent = 'Загрузить еще';
+        this.usersWrapper.append(this.loadMore);
+
+        this.main.append(this.usersWrapper)
+        this.app.append(this.title);
+        this.app.append(this.searchLine);
+        this.app.append(this.main);
+
+    }
+    createElement(elementTag, elementClass) {
+        const element = document.createElement(elementTag);
+        if (elementClass) {
+            element.classList.add(elementClass);
+        }
+        return element;
+    }
+    createUser(userData) {
+        const userElement = this.createElement('li', 'user-prev');
+        userElement.innerHTML = `<img class="user-prev-photo" src="${userData.avatar_url}" "alt="${userData.login}">
+        <span class="user-prev-name">${userData.login}</span>`;
+        this.usersList.append(userElement);
+
+    }
+}
+
+
